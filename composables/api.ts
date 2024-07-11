1fac2430af3f7c0a0081d70b199fcac7ef1e191f
Mahmut Sarıkaya <msrky1@gmail.com>
2024-07-11 18:30:30 +0300

    Deneme

--- composables/api.ts
+++ composables/api.ts
@@ -1,3 +1,3 @@
-// export const api  = "https://menu.tatliparkyaparsa.com/"
+export const api  = "https://menu.tatliparkyaparsa.com/"
 
-export const api  = "http://localhost:8000/"
+// export const api  = "http://localhost:8000/"
